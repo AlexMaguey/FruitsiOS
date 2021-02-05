51736d0b7fb210efd51f87659c1a6cb9bdfa7df5
alejandro.maguey <alejandro.maguey@wizeline.com>
2021-02-04 21:43:55 -0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Alejandro Maguey Renteria on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
